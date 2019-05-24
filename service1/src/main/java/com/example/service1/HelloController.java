dcfa216e22269510fc1deccd748cab0a443f2119
zeroual <abid1991@hotmail.fr>
2019-05-24 14:01:12 +0200

    step6

--- service1/src/main/java/com/example/service1/HelloController.java
+++ service1/src/main/java/com/example/service1/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-            String name = restTemplate.getForObject("http://localhost:8002/name", String.class);
+            String name = restTemplate.getForObject("http://service2-service:8002/name", String.class);
         return "Hello " + name;
     }
 }
